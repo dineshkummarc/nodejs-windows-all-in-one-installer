97e2a54244fabcc17e391eb8003a8de84b21186e
Sidney Lima Filho <sidney.filho@vivina.com.br>
2011-10-25 21:46:34 -0200

    fix

--- installer/install.bat
+++ installer/install.bat
@@ -1,13 +1,24 @@
+:: PATH Variable sample
+::C:\Windows;C:\Windows\system32;%CommonProgramFiles%\Microsoft Shared\Windows Live;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Program Files\Intel\DMIX;C:\Program Files (x86)\Microsoft SQL Server\90\Tools\binn\;C:\Program Files (x86)\Microsoft SQL Server\100\Tools\Binn\;C:\Program Files\Microsoft SQL Server\100\Tools\Binn\;C:\Program Files (x86)\Microsoft SQL Server\100\DTS\Binn\;C:\Program Files\TortoiseSVN\bin;C:\Program Files\TortoiseGit\bin;C:\Windows\Microsoft.NET\Framework\v2.0.50727;
+
 :: Set a local variable
 cd ..
 SET NODE_PATH=%CD%
 cd installer 
 
 :: Set a enviroment variable
-SETX /M "NODE_PATH" "%NODE_PATH%"
 SETX /M "NODE_ENV" "Development"
+SETX /M "NODE_PATH" "%NODE_PATH%"
+SETX /M "PATH" "%PATH%;%%GIT_PATH%%;%%NODE_PATH%%"
+
+
+@REG ADD "HKEY_CURRENT_USER\Software\Classes\joyent.node.js\shell\open\command" /ve /d "%%NODE_PATH%%\node.exe %1" /f
+@REG ADD "HKEY_CURRENT_USER\Software\Classes\.njs" /ve /d "joyent.node.js" /f
+
 
 :: install and start the service
 NSSM INSTALL "NodeJS" "%NODE_PATH%\node.exe" "%NODE_PATH%\server.js"
 NET START "NodeJS"
+
+
 
--- node-debug.bat
+++ node-debug.bat
@@ -1,19 +1,15 @@
-:: Set a local variable
-SET NODE_PATH=%CD%
-
 :: Set a enviroment variable
-SETX /M "NODE_PATH" "%CD%"
 SETX /M "NODE_ENV" "Development"
 
 :: kill process before opened
 TASKKILL /f /t /fi "imagename eq node*"
 
 :: Start
-START NODE --debug-brk server.js 
+START NODE --debug-brk "%NODE_PATH%\server.js"
 :: If inspector run before that server is done, he abort.
 ::SLEEP 1 
 :: Debugger
-START NODE "node_modules\node-inspector\bin\inspector.js"
+START NODE "%NODE_PATH%\node_modules\node-inspector\bin\inspector.js"
 :: Browser
 START http://localhost:8080
 
