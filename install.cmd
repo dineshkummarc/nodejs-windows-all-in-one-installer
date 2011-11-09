a7bd5df25c4c269dd9b94c274054f8a2ac5d646a
Sidney Lima Filho <sidney.filho@vivina.com.br>
2011-11-09 14:35:58 -0200

    Rename file

--- install.cmd
+++ install.cmd
@@ -1,12 +1,9 @@
 :: PATH Variable sample
 ::C:\Windows;C:\Windows\system32;%CommonProgramFiles%\Microsoft Shared\Windows Live;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Program Files\Intel\DMIX;C:\Program Files (x86)\Microsoft SQL Server\90\Tools\binn\;C:\Program Files (x86)\Microsoft SQL Server\100\Tools\Binn\;C:\Program Files\Microsoft SQL Server\100\Tools\Binn\;C:\Program Files (x86)\Microsoft SQL Server\100\DTS\Binn\;C:\Program Files\TortoiseSVN\bin;C:\Program Files\TortoiseGit\bin;C:\Windows\Microsoft.NET\Framework\v2.0.50727;
 
-:: Reset drive location
-c:
-
 :: Set a local variable, using program files because of iisnode
-if exist "%programfiles%" set NODE_PATH="%programfiles%\nodejs"
-if exist "%programfiles(x86)%" set NODE_PATH="%programfiles(x86)%\nodejs"
+if exist "%programfiles%" set NODE_PATH=%programfiles%\nodejs
+if exist "%programfiles(x86)%" set NODE_PATH=%programfiles(x86)%\nodejs
 if not exist "%NODE_PATH%" md "%NODE_PATH%"
 
 :: Set a enviroment variable
@@ -15,10 +12,10 @@ SETX /M "NODE_PATH" "%NODE_PATH%"
 SETX /M "PATH" "%PATH%;%%GIT_PATH%%;%%NODE_PATH%%"
 
 :: Associate NJS extension to node.exe
-FTYPE NodeJS.File.NJS="%NODE_PATH%\node" "%1"
+FTYPE NodeJS.File.NJS="%NODE_PATH%\node" "%%1"
 ASSOC .NJS=NodeJS.File.NJS
 
-
+copy /Y "node.exe" "%NODE_PATH%"
 :: install and start the service
 ::NSSM INSTALL "NodeJS" "%NODE_PATH%\node.exe" "%NODE_PATH%\server.js"
 ::NET START "NodeJS"
